74b775f3da2c0445cee8cf6e9b2ee64eb66e133d
203462 <77992679+203462@users.noreply.github.com>
2022-09-29 14:28:25 -0500

    finished exam

---
+++ examen/examen_203462.py
@@ -0,0 +1,29 @@
+import threading
+
+locked = threading.Lock()
+
+
+def mesa(persona):
+    global x
+    x = 2
+    if x == 2:
+        print("-------------------------------")
+        print("La persona " + str(persona) + " tiene " + str(x) + " palillos y ha empezado a comer")
+
+
+class Hilo(threading.Thread):
+    def __init__(self, persona):
+        threading.Thread.__init__(self)
+        self.persona = persona
+
+    def run(self):
+        locked.acquire()
+        mesa(self.persona)
+        locked.release()
+        print("dejo de comer")
+
+
+hilos = [Hilo(1), Hilo(2), Hilo(3), Hilo(
+    4), Hilo(5), Hilo(6), Hilo(7), Hilo(8)]
+for h in hilos:
+    h.start()  
